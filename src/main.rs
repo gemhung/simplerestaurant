3419add24d8df5fbf52e8ffe818240e8bb31233b
gemhung <gembright.stone.hung@gmail.com>
2024-12-02 09:31:54 +0900

    add telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 mod config;
+mod boot;
+mod errors;
 
 use config::settings;
-fn main() {
-    settings::init_settings();
-    println!("Hello, world!");
+
+fn main() -> Result<(), crate::errors::AppError> {
+    boot::telemetry::load();
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            {
+                let _ = config::settings::init_settings();
+                //let _ = aws::init_aws().await;
+                let _ = boot::database::init().await;
+                //let _ = boot::redis::init().unwrap();
+            }
+            // Launching web server
+            //config::boot::launch().await
+            todo!()
+        });
+
+    Ok(())
 }
+
+
