f5d1f189ea3ed1b415afa59ee2155a27deb52fc5
gemhung <gembright.stone.hung@gmail.com>
2024-12-04 16:45:58 +0900

    remove redundant errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,16 @@
 #![allow(clippy::result_large_err)]
 use actix_web::http::StatusCode;
-
 use crate::config::settings;
 
 #[allow(unused)]
 #[derive(thiserror::Error, Debug, strum::AsRefStr)]
 pub enum InnerAppError {
+    #[error("{self:?}")]
+    MissingFilter(String),
+
+    #[error("{self:?}")]
+    InvalidFilter(String),
+
     #[error("{self:?}")]
     InvalidParameter(String),
 
@@ -167,42 +172,19 @@ impl AppError {
             location: std::panic::Location::caller(),
         }
     }
+
     #[track_caller]
-    pub fn downcast(err: impl Into<String>) -> Self {
+    pub fn missing_filter(err: impl Into<String>) -> Self {
         AppError {
-            message: InnerAppError::Downcast(err.into()),
+            message: InnerAppError::MissingFilter(err.into()),
             location: std::panic::Location::caller(),
         }
     }
 
     #[track_caller]
-    pub fn empty_content_type(err: impl Into<String>) -> Self {
+    pub fn invalid_filter(err: impl Into<String>) -> Self {
         AppError {
-            message: InnerAppError::EmptyContentType(err.into()),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn unescape(err: impl Into<String>) -> Self {
-        AppError {
-            message: InnerAppError::Unescape(err.into()),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn into_sea_query_value(err: impl Into<String>) -> Self {
-        AppError {
-            message: InnerAppError::IntoSeaQueryValue(err.into()),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn another_user_already_login(user_name: impl Into<String>) -> Self {
-        AppError {
-            message: InnerAppError::AnotherUserAlreadyLogin(user_name.into()),
+            message: InnerAppError::InvalidFilter(err.into()),
             location: std::panic::Location::caller(),
         }
     }
@@ -215,233 +197,17 @@ impl AppError {
         }
     }
 
-    #[track_caller]
-    pub fn message<E: Into<InnerAppError>>(msg: E) -> Self {
-        AppError {
-            message: msg.into(),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn no_such_slug(s: String) -> Self {
-        AppError {
-            message: InnerAppError::NoSuchSlug(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn no_such_primary_key(s: String) -> Self {
-        AppError {
-            message: InnerAppError::NoSuchPrimaryKey(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn text_validation(s: String) -> Self {
-        AppError {
-            message: InnerAppError::TextValidationError(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn missing_item_value(id: i32) -> Self {
-        AppError {
-            message: InnerAppError::MissingItemValue(id),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn missing_attribute(s: String) -> Self {
-        AppError {
-            message: InnerAppError::MissingAttribute(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn validation(s: String) -> Self {
-        AppError {
-            message: InnerAppError::Validation(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn phone_validation(s: String) -> Self {
-        AppError {
-            message: InnerAppError::PhoneValidationError(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn invalid_attribute(s: String) -> Self {
-        AppError {
-            message: InnerAppError::InvalidAttributes(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn invalid_domain(s: String) -> Self {
-        AppError {
-            message: InnerAppError::InvalidDomain(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn missing_subdomain(s: String) -> Self {
-        AppError {
-            message: InnerAppError::MissingSubdomain(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn web_site_not_found(s: String) -> Self {
-        AppError {
-            message: InnerAppError::WebSiteNotFound(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn not_allowed_anonymous(s: String) -> Self {
-        AppError {
-            message: InnerAppError::NotAllowAnonymous(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn change_password(s: String) -> Self {
-        AppError {
-            message: InnerAppError::ChangePasswordError(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn token_not_exist() -> Self {
-        AppError {
-            message: InnerAppError::TokenNotExist,
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn unknown_url_for_service(service: String) -> Self {
-        AppError {
-            message: InnerAppError::UnknownUrlForService(service),
-            location: std::panic::Location::caller(),
-        }
-    }
-
-    #[track_caller]
-    pub fn metadata_parser(s: String) -> Self {
-        AppError {
-            message: InnerAppError::MetaDataParserErorr(s),
-            location: std::panic::Location::caller(),
-        }
-    }
-    #[track_caller]
-    pub fn no_such_view(id: i64) -> Self {
-        AppError {
-            message: InnerAppError::NoSuchView(id),
-            location: std::panic::Location::caller(),
-        }
-    }
-    #[track_caller]
-    pub fn cant_find_timestamp(json: String) -> Self {
-        AppError {
-            message: InnerAppError::CantFindTimestamp(json),
-            location: std::panic::Location::caller(),
-        }
-    }
-    #[track_caller]
-    pub fn invalid_status_code(msg: String) -> Self {
-        AppError {
-            message: InnerAppError::InvalidStatusCode(msg),
-            location: std::panic::Location::caller(),
-        }
-    }
-    #[track_caller]
-    pub fn bad_request(msg: impl Into<String>) -> Self {
-        AppError {
-            message: InnerAppError::BadRequest(msg.into()),
-            location: std::panic::Location::caller(),
-        }
-    }
-    #[track_caller]
-    pub fn no_such_method(msg: String) -> Self {
-        AppError {
-            message: InnerAppError::NoSuchMethod(msg),
-            location: std::panic::Location::caller(),
-        }
-    }
-    #[track_caller]
-    pub fn no_such_route(msg: String) -> Self {
-        AppError {
-            message: InnerAppError::NoSuchRoute(msg),
-            location: std::panic::Location::caller(),
-        }
-    }
-
     fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
         tracing::error!("{self}");
         use actix_web::ResponseError;
         let mut builder = actix_web::HttpResponseBuilder::new(self.status_code());
         match self {
-            // InvalidDomain
-            AppError {
-                message: InnerAppError::InvalidDomain(_msg),
-                ..
-            } => {
-                //let data = serde_json::json!({
-                //    "settings": settings().serialize_for_view(""),
-                //});
-                //let rendered =
-                //    crate::template_engine::render("errors/invalid_domain", "ignore", Some(data))
-                //        .unwrap_or_default();
-
-                builder
-                    .status(actix_web::http::StatusCode::FORBIDDEN)
-                    .body(())
-            }
-            // WebSiteNotFound
-            AppError {
-                message: InnerAppError::WebSiteNotFound(_msg),
-                ..
-            } => {
-                //let rendered =
-                //    crate::template_engine::render("errors/web_site_not_found", "ignore", None)
-                //        .unwrap_or_default();
-                builder
-                    .status(actix_web::http::StatusCode::NOT_FOUND)
-                    .body(())
-            }
-            // NoSuchRoute
-            AppError {
-                message: InnerAppError::NoSuchRoute(_msg),
-                ..
-            } => builder
-                .status(actix_web::http::StatusCode::BAD_REQUEST)
-                .body(_msg.clone()),
-            // TODO: Redirect all errors to "/dashboard" temporarily
             AppError { message, .. } => builder
                 .status(actix_web::http::StatusCode::BAD_REQUEST)
                 .body(format!("{:?}", message)),
         }
     }
 }
-
-pub type CMSResult<T> = Result<T, AppError>;
 
 impl<E: Into<InnerAppError>> From<E> for AppError {
     #[track_caller]
