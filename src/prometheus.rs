bf1c962f43ac51d30434a05040468c6886db9823
gemhung <gembright.stone.hung@gmail.com>
2025-01-08 12:13:59 +0900

    add cpu usage

--- src/prometheus.rs
+++ src/prometheus.rs
@@ -10,8 +10,10 @@ pub fn prometheus() -> actix_web_prom::PrometheusMetrics {
         "mem_usage",
         "Average memory usage (1-100)",
         Box::new(move || {
-            let mut sys = sysinfo::System::new();
-            sys.refresh_memory_specifics(sysinfo::MemoryRefreshKind::nothing().with_ram());
+            let sys = sysinfo::System::new_with_specifics(
+                sysinfo::RefreshKind::nothing()
+                    .with_memory(sysinfo::MemoryRefreshKind::nothing().with_ram()),
+            );
             let used = sys.used_memory();
             let total = sys.total_memory();
             if total > 0 {
@@ -25,6 +27,27 @@ pub fn prometheus() -> actix_web_prom::PrometheusMetrics {
     prometheus
         .registry
         .register(Box::new(mem_usage.clone()))
+        .unwrap();
+    let cpu_usage = prometheus::PullingGauge::new(
+        "cpu_usage",
+        "Average cpu usage (1-100)",
+        Box::new(move || {
+            use sysinfo::{CpuRefreshKind, RefreshKind, System};
+            let mut s = System::new_with_specifics(
+                RefreshKind::nothing().with_cpu(CpuRefreshKind::nothing().with_cpu_usage()),
+            );
+            // Wait a bit because CPU usage is based on diff.
+            std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+            // Refresh CPUs again to get actual value.
+            s.refresh_cpu_usage();
+
+            s.global_cpu_usage().trunc() as f64
+        }),
+    )
+    .unwrap();
+    prometheus
+        .registry
+        .register(Box::new(cpu_usage.clone()))
         .unwrap();
 
     prometheus
